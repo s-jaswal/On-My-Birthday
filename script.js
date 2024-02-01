8ab60ffe9a15506796cbd0c10ca3d674195b22e7
s-jaswal <sjasdocs@gmail.com>
2024-02-01 05:29:05 -0500

    Inputted all content info into assigned button

--- script.js
+++ script.js
@@ -5,16 +5,68 @@ function displayMessage(buttonNumber) {
             message = "Just after midnight on September 27, 1869, Ellis County Sheriff Wild Bill Hickok and his deputy respond to a report that a local ruffian named Samuel Strawhun and several drunken buddies were tearing up John Bitters Beer Saloon in Hays City, Kansas. When Hickok arrived and ordered the men to stop, Strawhun turned to attack him, and Hickok shot him in the head. Strawhun died instantly, as did the riot. Such were Wild Bill's less-than-restrained law enforcement methods. Famous for his skill with a pistol and steely-calm under fire, James Butler Hickok initially seemed to be the ideal man for the sheriff of Ellis County, Kansas. The good citizens of Hays City, the county seat, were tired of the wild brawls and destructiveness of the hard-drinking buffalo hunters and soldiers who took over their town every night. They hoped the famous “Wild Bill” could restore peace and order, and in the late summer of 1869, elected him as interim county sheriff. Tall, athletic and sporting shoulder-length hair and a sweeping mustache, Hickok cut an impressive figure, and his reputation as a deadly shot with either hand was often all it took to keep many potential lawbreakers on the straight and narrow. As one visiting cowboy later recalled, Hickok would stand “with his back to the wall, looking at everything and everybody under his eyebrows—just like a mad old bull.” But when Hickok applied more aggressive methods of enforcing the peace, some Hays City citizens wondered if their new cure wasn't worse than the disease. Shortly after becoming sheriff, Hickok shot a belligerent soldier who resisted arrest, and the man died the next day. A few weeks later Hickok killed Strawhun. While his brutal ways were indisputably effective, many Hays City citizens were less than impressed that after only five weeks in office he had already found it necessary to kill two men in the name of preserving peace. During the regular November election later that year, the people expressed their displeasure, and Hickok lost to his deputy, 144-89. Though Wild Bill Hickok would later go on to hold other law enforcement positions in the West, his first attempt at being a sheriff had lasted only three months.";
             break;
         case 2:
-            message = "Your custom message for button 2";
+            message = "On September 27, 1938, President Franklin Roosevelt wrote to German Chancellor Adolf Hitler regarding the threat" +
+      " of war in Europe. The German chancellor had been threatening to invade the Sudetenland of Czechoslovakia and, in" +
+      " the letter, his second to Hitler in as many days, Roosevelt reiterated the need to find a peaceful resolution to" +
+      " the issue." +
+      " The previous day, FDR had written to Hitler with an appeal to negotiate with Czechoslovakia regarding Germany's" +
+      " desire for the natural and industrial resources of the Sudetenland rather than resort to force. Hitler responded" +
+      " that Germany was entitled to the area because of the “shameful” way in which the Treaty of Versailles, which had" +
+      " ended World War I, had made Germany a “pariah” in the community of nations. The treaty had given the Sudetenland," +
+      " a territory that was believed by Hitler and many of his supporters to be inherently German, to the state of" +
+      " Czechoslovakia. Therefore, Hitler reasoned, German invasion of the Sudetenland was justified, as annexation by" +
+      " Germany would simply mean returning the area to its cultural and historical roots. Hitler assured Roosevelt that" +
+      " he also desired to avoid another large-scale war in Europe." +
+      " In his letter of September 27, Roosevelt expressed relief at Hitler's assurances but re-emphasized his desire that" +
+      " “negotiations [between Germany and Czechoslovakia] be continued until a peaceful settlement is found.” FDR also" +
+      " suggested that a conference of all nations concerned with the current conflict be convened as soon as possible. He" +
+      " appealed to Hitler's ego, saying “should you agree to a solution in this peaceful manner I am convinced that" +
+      " hundreds of millions throughout the world would recognize your action as an outstanding historic service to all" +
+      " humanity.” FDR then assured Hitler that the U.S. would remain neutral regarding European politics, but that" +
+      " America recognized a responsibility to be involved “as part of a world of neighbors.”" +
+      " In the end, Hitler ignored the international community's pleas for a peaceful solution and invaded Czechoslovakia" +
+      " in March 1939. The invasion was just the first in Hitler's quest to control Europe and create a “Third Reich” of" +
+      " German geopolitical supremacy.";
             break;
         case 3:
-            message = "Your custom message for button 3";
+            message = "Zsa Zsa Gabor was a celebrity and socialite for decades mostly for being herself. She acted in films with Fred Allen," +
+      " José Ferrer and Orson Welles. Her television resume included Gilligan's Island, Batman, The Love Boat and countless talk" +
+      " show appearances. Gabor was married nine times, most recently to Prince Frederick von Anhalt. On December 18, 2016, she" +
+      " died at the age of 99. Sari Gabor was born on February 6, 1917 (some sources say 1918), in Budapest, Hungary, the middle" +
+      " daughter of Vilmos Gabor, a soldier, and Jolie Gabor, the heiress to a European jewelry business. Gabor and her two" +
+      " sisters, Eva and Magda, lived a life of luxury, which included a staff of servants, extensive vacations and stints at" +
+      " expensive boarding schools.  What audiences seemed most" +
+      " interested in, however, was Zsa Zsa's personal life. To many, she appeared as an icon of European glamour, luxury and" +
+      " self-indulgence. Often portrayed as a wily seductress, she frequently appeared on television as an alluring, witty and" +
+      " sometimes challenging guest who had a habit of calling nearly everyone. But as a vivacious and dramatic" +
+      " personality, Gabor quickly became tabloid fodder, more famous for her marriages and conspicuous wealth than for her" +
+      " acting abilities. On December 18, 2016, Gabor died of heart failure at the age of 99." +
+      " In the spring of 2018, it was announced that nearly 1,000 items from Gabor's estate would go on the block at the Hello" +
+      " Dah-lings auction in April. The list included a 104-piece Champagne set, a saddle given to the actress by Ronald Reagan" +
+      " and some of her favorite jewelry, though the top-selling item turned out to be a Margaret Keane portrait of Gabor, which" +
+      " went for $45,000.";
             break;
         case 4:
-            message = "Your custom message for button 4";
+            message = "September 27, 1998 is the date that Google now recognizes as its birthday, but that isn't the actual day they first" +
+      " appeared online. In fact, they seem to have changed their own birthday several times over the years." +
+      " One of the best examples of this was Google's fifth birthday, which was celebrated on September 8, 2003, instead of" +
+      " September 27 as it was the year before. But, on Google's sixth birthday in 2004 they chose to celebrate on September 7." +
+      " One year later in 2005 they celebrated their 7th birthday on September 26. By the next year they were back to September" +
+      " 27 where it has remained ever since. Despite first appearing online September 8, 1998, Google now recognizes September" +
+      " 27, 1998, as their official birthday and has done so since at least 2005. So how did the date get changed to the 27th?" +
+      " Good question! If you want to get technical, Google was officially incorporated on September 4, 1998, so they could have" +
+      " selected that date. Alternatively, they also could have chosen September 15, 1997 - the day that the Google.com domain" +
+      " name was registered for the first time. But instead Google selected September 27 as their birthday in order to celebrate" +
+      " the number of pages that were being indexed on the search engine - it was a marketing strategy!";
             break;
         case 5:
-            message = "Your custom message for button 5";
+            message = "Donald Trump and his sons were found liable for fraud and canceled the Trump's Organization business certification." +
+      " A New York judge has found former President Donald Trump and his adult sons liable for fraud and canceled the Trump" +
+      " Organization's business certification, saying the Trumps provided false financial statements for roughly a decade. The" +
+      " civil case is a blow to Trump and a complete rejection of his arguments that he didn't inflate the values of his golf" +
+      " courses, hotels and homes on financial statements that were repeatedly used in business. The New York attorney general" +
+      " has sought $250 million in damages, a ban on the Trumps from serving as officers of a business in the state, and to stop" +
+      " the company from engaging in business transactions for five years. The trial's start date remains in limbo.";
             break;
         default:
             message = "Unknown button clicked.";
